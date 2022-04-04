f21a86432712b5c65f9c4892755bb4a2aa40a4df
ariumaschi <aumaschi@udesa.edu.ar>
2022-04-04 17:40:55 -0300

    product index

--- proyecto/controllers/productController.js
+++ proyecto/controllers/productController.js
@@ -3,19 +3,19 @@ var data = require('../db/data');
 const productController = {
 
     index: function(req, res, next){
-        res.send(data.products);
+        res.render('product_index', {products: data.products});
     },
 
     search: function(req, res) {
         if (req.query) {
-            res.send (data.findProductsBy(req.query));
+            res.render ('search_results',{results: data.findProductsBy(req.query)});
         }
     },
 
     show: function(req, res) {
         const result = data.lista[req.params.id - 1];
         if (!result) {
-            res.send('No hay ningun producto con el ID ' + req.params.id);
+            res.render('product_detail', {detail: 'No hay ningun producto con el ID ' + req.params.id});
         }
         res.send(result);
     },
