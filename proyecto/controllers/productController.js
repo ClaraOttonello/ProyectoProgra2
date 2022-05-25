e31eb6f220b4e0d5cb7ac8698326f9c58ab2af86
Ariana Umaschi <aumaschi@udesa.edu.ar>
2022-05-25 19:25:16 -0300

    cambios varios: product controller, prodcut detail...

--- proyecto/controllers/productController.js
+++ proyecto/controllers/productController.js
@@ -19,7 +19,7 @@ const productController = {
     show: function(req, res) {
         product.findByPk(req.params.id)
             .then(function (products) {
-                res.render('search_results', { products });
+                res.render('product_detail', { products });
             })
             .catch(function (error) {
                 res.send(error);
@@ -31,14 +31,43 @@ const productController = {
     },
 
     store: function(req, res) {
-        db.Product.create(req.body)
+        //cuando avancemos con session agregar:   req.body.user_id = req.session.user.id;
+        product.create(req.body)
             .then(function() {
                 res.redirect('/')
             })
             .catch(function(error) {
                 res.send(error);
             })
-    }
+    },
+
+    delete: function(req, res) {
+        product.destroy({ where: { id: req.params.id } })
+            .then(function() {
+                res.redirect('/')
+            })
+            .catch(function(error) {
+                res.send(error);
+            })
+    },
+    edit: function(req, res) {
+       product.findByPk(req.params.id)
+            .then(function (product) {
+                res.render('product_edit', { product });
+            })
+            .catch(function (error) {
+                res.send(error);
+            })
+    },
+    update: function(req, res) {
+        product.update(req.body, { where: { id: req.params.id } })
+            .then(function(product) {
+                res.redirect('/')
+            })
+            .catch(function(error) {
+                res.send(error);
+            })
+    },
 }
 
 module.exports = productController;
--- proyecto/routes/products.js
+++ proyecto/routes/products.js
@@ -4,8 +4,11 @@ var controller = require('../controllers/productController')
 
 router.get('/', controller.index);
 router.get('/detail/:id', controller.show);
+router.get('/:id/edit', controller.edit);
+router.post('/:id/edit', controller.update);
 router.get('/add', controller.add);
 router.post('/add', controller.store);
+router.post('/:id/delete', controller.delete);
 router.get('/:id', controller.show);
 
 module.exports = router;
