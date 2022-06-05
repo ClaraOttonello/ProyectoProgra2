9df818366d28a346013c8a9830c2fd7a04ced916
Ariana Umaschi <aumaschi@udesa.edu.ar>
2022-06-05 18:45:09 -0300

    carga de productos: controlador, ruta, vista

--- proyecto/controllers/productController.js
+++ proyecto/controllers/productController.js
@@ -30,7 +30,13 @@ const productController = {
     },
 
     store: function(req, res) {
-        //cuando avancemos con session agregar:   req.body.user_id = req.session.user.id;
+        req.send (req.files);
+        req.body.user_id = req.session.user.id;
+        if (req.file) req.body.image = (req.file.path).replace('public', '');
+        //image es el nombre del campo del formulario que carga la imagen
+        //para que venga el req.file primero le pusimos al formulario el enctype="multipart/form-data" 
+        //en req.file hay muchas propiedades y la mas importante es el path que tiene la ruta completa en donde esta el archivo
+        //el path lo aclaramos nosotoros en la carpeta destin   ation en la ruta
         product.create(req.body)
             .then(function() {
                 res.redirect('/')
--- proyecto/routes/products.js
+++ proyecto/routes/products.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/productController')
+var multer = require('multer');
+const upload = multer({ dest: 'public/images/uploads' });
 
 router.get('/', controller.index);
 router.get('/detail/:id', controller.show);
+
 router.get('/:id/edit', controller.edit);
-router.post('/:id/edit', controller.update);
+router.post('/:id/edit', controller.update); //falta algo aca
+
 router.get('/add', controller.add);
-router.post('/add', controller.store);
+router.post('/add', upload.single('image'), controller.store);
+//upload.single es un middleware que va entre la ruta y el controlador como intermedio, 'image' es el name del campo del formulario
+
 router.post('/:id/delete', controller.delete);
+
 router.get('/:id', controller.show);
 
 module.exports = router;
